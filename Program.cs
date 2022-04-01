378e148ded8248f975705dabf18fbe61f8ed93fb
tcantello <tcantello@my.lonestar.edu>
2022-04-01 13:17:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Program - week10Assignment
+Programer - Toby Cantello
+Date Created - 3/31/2022
+Last Date Updated - 3/31/2022
+*/
+
+using System;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace week10Assignment
+{
+    class Fighter
+    {
+        int health = 20;
+    }
+
+    class Enemy
+    {
+
+    }
+
+    class Player
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO COMBAT!!");
+        }
+    }
+}
